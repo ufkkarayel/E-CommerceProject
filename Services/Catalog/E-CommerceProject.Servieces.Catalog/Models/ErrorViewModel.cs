d79959430d88752a126d226de677d89cc8e5f1af
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-13 21:25:10 +0300

    ..

--- Services/Catalog/E-CommerceProject.Servieces.Catalog/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace E_CommerceProject.Servieces.Catalog.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
