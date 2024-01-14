6981737bd63deed1801e483e72dbdc18d939ed4e
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 15:25:05 +0300

    .

--- Services/Catalog/E-CommerceProject.Services.Catalog/Models/Product.cs
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Models/Product.cs
@@ -1,6 +1,21 @@
-﻿namespace E_CommerceProject.Services.Catalog.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace E_CommerceProject.Services.Catalog.Models
 {
     public class Product
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public string ProductImage { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+
     }
 }
