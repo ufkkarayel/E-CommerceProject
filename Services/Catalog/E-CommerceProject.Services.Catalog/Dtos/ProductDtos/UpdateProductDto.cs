769b4bd6028a92db76acb1f027ff8b79776078df
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 15:34:00 +0300

    Dtolar tamamlandı

---
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Dtos/ProductDtos/UpdateProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace E_CommerceProject.Services.Catalog.Dtos.ProductDtos
+{
+    public class UpdateProductDto
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public string ProductImage { get; set; }
+        public string CategoryID { get; set; }
+    }
+}
