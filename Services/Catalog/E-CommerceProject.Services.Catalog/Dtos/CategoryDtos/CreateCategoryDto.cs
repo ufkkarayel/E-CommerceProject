31d4ab114b390936055a48247d4cfa5916887d21
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 15:31:07 +0300

    create  ve update dtolar eklendi

---
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace E_CommerceProject.Services.Catalog.Dtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Dtos/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace E_CommerceProject.Services.Catalog.Dtos.CategoryDtos
+{
+    public class UpdateCategoryDto
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
