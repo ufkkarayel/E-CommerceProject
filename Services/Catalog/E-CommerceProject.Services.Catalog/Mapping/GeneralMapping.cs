7f33193c54ea8bb592d74caa3b72ea4f89203238
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 15:40:38 +0300

    automapper eklendi

---
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using E_CommerceProject.Services.Catalog.Dtos.CategoryDtos;
+using E_CommerceProject.Services.Catalog.Dtos.ProductDtos;
+using E_CommerceProject.Services.Catalog.Models;
+
+namespace E_CommerceProject.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>();
+            CreateMap<ResultCategoryDto, Category>();
+
+            CreateMap<Category,CreateCategoryDto>().ReverseMap();
+            CreateMap<Category,UpdateCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product,CreateProductDto>().ReverseMap();
+            CreateMap<Product,UpdateProductDto>().ReverseMap();
+        }
+    }
+}
