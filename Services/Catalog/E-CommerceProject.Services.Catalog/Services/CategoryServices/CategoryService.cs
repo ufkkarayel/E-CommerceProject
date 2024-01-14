26fdff717c68ec89355169e1f9cda72228bbad56
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 17:58:01 +0300

    ..

--- Services/Catalog/E-CommerceProject.Services.Catalog/Services/CategoryServices/CategoryService.cs
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Services/CategoryServices/CategoryService.cs
@@ -1,6 +1,47 @@
-﻿namespace E_CommerceProject.Services.Catalog.Services.CategoryServices
+﻿using AutoMapper;
+using E_CommerceProject.Services.Catalog.Dtos.CategoryDtos;
+using E_CommerceProject.Services.Catalog.Models;
+using E_CommerceProject.Services.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace E_CommerceProject.Services.Catalog.Services.CategoryServices
 {
-    public class CategoryService
+    public class CategoryService : ICategoryService
     {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultCategoryDto> GetCategoryById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
