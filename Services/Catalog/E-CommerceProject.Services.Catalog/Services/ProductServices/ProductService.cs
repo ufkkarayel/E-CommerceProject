eadf8d332f12d2c71b962d8a1d7781a2bf7d8a54
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 21:41:34 +0300

    ..

--- Services/Catalog/E-CommerceProject.Services.Catalog/Services/ProductServices/ProductService.cs
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Services/ProductServices/ProductService.cs
@@ -1,6 +1,47 @@
-﻿namespace E_CommerceProject.Services.Catalog.Services.ProductServices
+﻿using AutoMapper;
+using E_CommerceProject.Services.Catalog.Dtos.ProductDtos;
+using E_CommerceProject.Services.Catalog.Models;
+using E_CommerceProject.Services.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace E_CommerceProject.Services.Catalog.Services.ProductServices
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly IMongoCollection<Product> _productCollection;
+        private readonly IMapper _mapper;
+
+        public ProductService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _productCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName);
+            _mapper = mapper;
+        }
+
+        public Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultProductDto> GetProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
