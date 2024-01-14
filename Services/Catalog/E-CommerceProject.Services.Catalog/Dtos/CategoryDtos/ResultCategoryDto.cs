e331ca3475cc18db15eeb659a323fa0cdcc04d4e
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 15:30:10 +0300

    resuılt category dto eklendi

---
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace E_CommerceProject.Services.Catalog.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
