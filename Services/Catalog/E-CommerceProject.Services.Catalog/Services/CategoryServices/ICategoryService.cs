53bd45bbfe6c6efd8658053a50624e58faad7cd1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 17:51:27 +0300

    ..

--- Services/Catalog/E-CommerceProject.Services.Catalog/Services/CategoryServices/ICategoryService.cs
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -5,9 +5,9 @@ namespace E_CommerceProject.Services.Catalog.Services.CategoryServices
     public interface ICategoryService
     {
         Task<List<ResultCategoryDto>> GetAllCategoryAsync();
-        void CreateCategoryAsync(CreateCategoryDto createCategoryDto);
-        void UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
-        void DeleteCategoryAsync(string id);
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
         Task<ResultCategoryDto> GetCategoryById(string id);
     }
 }
--- Services/Catalog/E-CommerceProject.Services.Catalog/Services/ProductServices/IProductService.cs
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Services/ProductServices/IProductService.cs
@@ -1,6 +1,13 @@
-﻿namespace E_CommerceProject.Services.Catalog.Services.ProductServices
+﻿using E_CommerceProject.Services.Catalog.Dtos.ProductDtos;
+
+namespace E_CommerceProject.Services.Catalog.Services.ProductServices
 {
     public interface IProductService
     {
+        Task<List<ResultProductDto>> GetAllProductsAsync();
+        Task<ResultProductDto> GetProductAsync(string id);
+        Task CreateProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(string id);
     }
 }
