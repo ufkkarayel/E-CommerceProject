9e4fb1bc6bee51764d97b87e94b1db17cd229b18
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-14 15:50:03 +0300

    ..

---
+++ Services/Catalog/E-CommerceProject.Services.Catalog/Settings/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceProject.Services.Catalog.Settings
+{
+    public class DatabaseSettings
+    {
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
