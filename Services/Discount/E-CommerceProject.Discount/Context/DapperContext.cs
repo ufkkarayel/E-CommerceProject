4fd3b1d16b2766cdd31c77e72aefc5cb8a47de5b
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-15 22:22:30 +0300

    ..

---
+++ Services/Discount/E-CommerceProject.Discount/Context/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using E_CommerceProject.Discount.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace E_CommerceProject.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= (localdb)\\MSSQLLocalDB; initial Catalog=ECommerceDiscountDb; integrated Security= True;");
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ Services/Discount/E-CommerceProject.Discount/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace E_CommerceProject.Discount.Models
+{
+    public class Coupon
+    {
+        public int CouponID { get; set; }
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
+
+    }
+}
