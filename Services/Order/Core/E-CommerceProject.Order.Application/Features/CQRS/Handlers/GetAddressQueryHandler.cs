f4d6a08920e5d259b5e70ceb3957efaf13146d5c
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-16 22:26:59 +0300

    .

---
+++ Services/Order/Core/E-CommerceProject.Order.Application/Features/CQRS/Handlers/GetAddressQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using E_CommerceProject.Order.Application.Features.CQRS.Results;
+using E_CommerceProject.Order.Application.Interfaces;
+using E_CommerceProject.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceProject.Order.Application.Features.CQRS.Handlers
+{
+    public class GetAddressQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public GetAddressQueryHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetAddressByIdQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetAddressByIdQueryResult
+            {
+                AddressID = x.AddressID,
+                City = x.City,
+                Detail= x.Detail,
+                District = x.District,
+                UserID = x.UserID
+            }).ToList();
+        }
+    }
+}
