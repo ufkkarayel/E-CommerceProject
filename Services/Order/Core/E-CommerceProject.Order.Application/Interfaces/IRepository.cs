8979db5db4caf093b00e29d912d925ae8fa72703
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-16 22:08:29 +0300

    .

---
+++ Services/Order/Core/E-CommerceProject.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceProject.Order.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync();
+        Task<T> CreateAsync();
+        Task<T> UpdateAsync();
+        Task<T> DeleteAsync();
+        Task<List<T>> GetOrderByFilter(Expression<Func<T, bool>> filter);
+    }
+}
