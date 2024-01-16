e9cd056dfbaae50a17a31e72f22444fb35219053
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-16 20:10:48 +0300

    ..

--- Services/Discount/E-CommerceProject.Discount/Services/DiscountService.cs
+++ Services/Discount/E-CommerceProject.Discount/Services/DiscountService.cs
@@ -1,6 +1,46 @@
-﻿namespace E_CommerceProject.Discount.Services
+﻿using AutoMapper;
+using E_CommerceProject.Discount.Context;
+using E_CommerceProject.Discount.Dtos;
+using E_CommerceProject.Discount.Models;
+
+namespace E_CommerceProject.Discount.Services
 {
-    public class DiscountService
+    public class DiscountService : IDiscountService
     {
+        private readonly DapperContext _context;
+        private readonly IMapper _mapper;
+
+        public DiscountService(DapperContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task CreateCouponAsync(CreateCouponDto createCouponDto)
+        {
+            var values= _mapper.Map<Coupon>(createCouponDto);
+            await _context.Coupons.AddAsync(values);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultCouponDto> GetCouponsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCouponDto>> GetResultCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCouponAsync(UpdateCouponDto updateCouponDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
