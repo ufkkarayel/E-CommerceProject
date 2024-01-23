832e42ba449e35f5dc18ce0069ecbd9947b6b34f
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-23 20:30:43 +0300

    ..

---
+++ Services/Order/Core/E-CommerceProject.Order.Application/Features/Mediator/Handlers/GetOrderingByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using E_CommerceProject.Order.Application.Features.Mediator.Queries;
+using E_CommerceProject.Order.Application.Features.Mediator.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceProject.Order.Application.Features.Mediator.Handlers
+{
+    public class GetOrderingByIdQueryHandler : IRequestHandler<GetOrderingByIdQuery, GetOrderingByIdQueryResult>
+    {
+        public Task<GetOrderingByIdQueryResult> Handle(GetOrderingByIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
