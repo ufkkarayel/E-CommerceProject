6de2f36b1b21c26f69ac1d90646dbeb73be518cf
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-23 21:15:10 +0300

    ..

---
+++ Services/Order/Infrastructure/E-CommerceProject.Order.Persistance/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+﻿using E_CommerceProject.Order.Application.Interfaces;
+using E_CommerceProject.Order.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceProject.Order.Persistance.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly OrderContext _context;
+
+        public Repository(OrderContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetOrderByFilter(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
