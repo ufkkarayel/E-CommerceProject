42075e77f8f22441617677f208fa30ab0401f549
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-25 20:44:59 +0300

    ..

--- Services/Order/Presentation/E-CommerceProject.Order.WebApi/Program.cs
+++ Services/Order/Presentation/E-CommerceProject.Order.WebApi/Program.cs
@@ -1,6 +1,13 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
