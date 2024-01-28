98cc99d23e0eaed4beaafaf1ed50dc0f5380f0f1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-28 15:15:10 +0300

    ..

--- IdentityServer/ECommerceProject.IdentityServer/Dtos/UserRegisterDto.cs
+++ IdentityServer/ECommerceProject.IdentityServer/Dtos/UserRegisterDto.cs
@@ -2,5 +2,11 @@
 {
     public class UserRegisterDto
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+        public string Mail { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
