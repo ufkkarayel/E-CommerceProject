5776d32f7f9801531badd97d47b642c35844c5ae
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-02-02 21:16:04 +0300

    ..

---
+++ Services/Basket/ECommerceProject.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceProject.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int    Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/Basket/ECommerceProject.Basket/Dtos/BasketTotalDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceProject.Basket.Dtos
+{
+    public class BasketTotalDto
+    {
+        public string UserID { get; set; }
+        public string DiscountCode { get; set; }
+        public int DiscountRate { get; set; }
+        public List<BasketItemDto>  BasketItems { get; set; }
+        public decimal TotalPrice { get => BasketItems.Sum(x => x.Price * x.Quantity); }
+
+    }
+}
